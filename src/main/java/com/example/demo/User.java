50a33b8f339bc555906970b61ecf8254e9d6c704
Matt Davies <matt.davies400@gmail.com>
2025-11-29 13:39:13 -0500

    Pushing the important stuff

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class User {
+    private String name;
+    private int age;
+
+    // constructor
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getters
+    public String getName() { return name; }
+    public int getAge() { return age; }
+}
